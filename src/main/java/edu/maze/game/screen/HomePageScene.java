420369158648a54523871dbe10a1d0cba4c7601c
Reham Tarek <rehamtarek666666@gmail.com>
2021-04-11 21:15:42 +0200

    added GUI option to select generation algorithm

--- src/main/java/edu/maze/game/screen/HomePageScene.java
+++ src/main/java/edu/maze/game/screen/HomePageScene.java
@@ -12,6 +12,9 @@ import javafx.stage.Stage;
 
 public class HomePageScene {
     public final static int PLAY = 0, BOT_PLAY = 1;
+    private final String KRUSKAL = "Randomized Kruskal's Algorithm";
+    private final String DFS = "Randomized Depth First Search";
+    private final String PRIM = "Randomized Prim's Algorithm";
     private int rows = 8, cols = 8;
 
     private int getGridSize(String selected) {
@@ -24,7 +27,7 @@ public class HomePageScene {
 
     private VBox selectLevelComboBox() {
         VBox vBox = new VBox();
-        Label label = new Label("Please Select a level");
+        Label label = new Label("Please Select a Level");
 
         ComboBox level = new ComboBox();
         level.getItems().addAll("Easy", "Medium", "Hard");
@@ -32,11 +35,29 @@ public class HomePageScene {
         level.valueProperty().addListener((ChangeListener<String>) (observable, oldValue, newValue) -> {
             rows = cols = getGridSize(newValue);
         });
-        level.setMaxWidth(200);
+        level.setMaxWidth(230);
 
         vBox.setSpacing(10);
         vBox.setAlignment(Pos.CENTER);
         vBox.getChildren().addAll(label, level);
+        return vBox;
+    }
+
+    private VBox selectAlgorithmComboBox() {
+        VBox vBox = new VBox();
+        Label label = new Label("Please Select Maze Generation Algorithm");
+
+        ComboBox algorithm = new ComboBox();
+        algorithm.getItems().addAll(DFS, KRUSKAL, PRIM);
+        algorithm.setValue(DFS);//set initial state to Easy
+        algorithm.valueProperty().addListener((ChangeListener<String>) (observable, oldValue, newValue) -> {
+            /// todo
+        });
+        algorithm.setMaxWidth(230);
+
+        vBox.setSpacing(10);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.getChildren().addAll(label, algorithm);
         return vBox;
     }
 
@@ -52,20 +73,23 @@ public class HomePageScene {
 
     public Scene createHomePageScene(Stage stage) {
         stage.setTitle("Start Game");
-        VBox vBox = selectLevelComboBox();
-        Button startButton = createStartButton("Play Game", stage, PLAY);
-        Button botPlayButton = createStartButton("Bot Play", stage, BOT_PLAY);
+        VBox level = selectLevelComboBox();
+
+        VBox algorithm = selectAlgorithmComboBox();
 
         VBox vBox2 = new VBox();
         vBox2.setAlignment(Pos.CENTER);
         vBox2.getStyleClass().add("VBox");
+
         HBox hBox = new HBox();
         hBox.setSpacing(50);
         hBox.setAlignment(Pos.CENTER);
         hBox.setMaxWidth(500);
+        Button startButton = createStartButton("Play Game", stage, PLAY);
+        Button botPlayButton = createStartButton("Bot Play", stage, BOT_PLAY);
         hBox.getChildren().addAll(startButton, botPlayButton);
 
-        vBox2.getChildren().addAll(vBox, hBox);
+        vBox2.getChildren().addAll(level, algorithm, hBox);
 
         Scene scene = new Scene(vBox2, 940, 780);
         scene.getStylesheets().add("style.css");
