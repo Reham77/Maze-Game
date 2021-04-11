002c1d61c380bd9a42b33aa0df461edfe5cb74f7
Reham Tarek <rehamtarek666666@gmail.com>
2021-04-11 03:33:09 +0200

    Addes Kruskal Maze Builder functionality

---
+++ src/main/java/edu/maze/game/builder/KruskalMazeBuilder.java
@@ -0,0 +1,89 @@
+package edu.maze.game.builder;
+
+import edu.maze.game.entity.Board;
+import edu.maze.game.entity.Cell;
+import edu.maze.game.entity.Wall;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class KruskalMazeBuilder extends MazeBuilder {
+    private int[] parent = new int[(rows * cols) + 30];
+    private int[] sz = new int[(rows * cols) + 30];
+
+    private List<Wall> walls;
+
+    public KruskalMazeBuilder(int rows, int cols) {
+        super(rows, cols);
+    }
+
+    @Override
+    protected boolean validCell(Cell cell) {
+        return inBounds(cell);
+    }
+
+    private void swap(Integer u, Integer v) {
+        Integer temp = u;
+        u = v;
+        v = temp;
+    }
+
+    private void generateWalls() {
+        walls = new ArrayList<>();
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                walls.addAll(getValidNeighbours(new Cell(i, j)));
+            }
+        }
+        Collections.shuffle(walls);
+    }
+
+    private void init() {
+        for (int i = 1; i <= (rows * cols); ++i) {
+            parent[i] = i;
+            sz[i] = 1;
+        }
+        generateWalls();
+    }
+
+    private int root(int node) {
+        while (node != parent[node]) {
+            parent[node] = parent[parent[node]];
+            node = parent[node];
+        }
+        return node;
+    }
+
+    private boolean join(int u, int v) {
+        u = root(u);
+        v = root(v);
+        if (u == v)
+            return false;
+        if (sz[u] < sz[v])
+            swap(u, v);
+        sz[u] += sz[v];
+        parent[v] = u;
+        return true;
+    }
+
+    boolean find(int u, int v) {
+        return root(u) == root(v);
+    }
+
+    @Override
+    public Board build() {
+        Board board = new Board(rows, cols);
+        init();
+        for (Wall wall : walls) {
+            Cell a = wall.from;
+            Cell b = wall.to;
+
+            if (!find(a.getIdx(rows), b.getIdx(rows))) {
+                join(a.getIdx(rows), b.getIdx(rows));
+                board.removeWall(a, wall.direction);
+            }
+        }
+        return board;
+    }
+}
